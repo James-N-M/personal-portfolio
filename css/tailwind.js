a47dafb2595ea0f649165841742da397da0ae046
James Moore <jamesm0025@gmail.com>
2019-02-08 13:09:16 -0500

    added icons for information

--- css/tailwind.js
+++ css/tailwind.js
@@ -55,6 +55,7 @@ let colors = {
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
   'white': '#ffffff',
+  'white-dark': '#fdf9f9',
 
   'red-darkest': '#3b0d0c',
   'red-darker': '#621b18',
